4dc4acd5c8570aa8bc5ce559c15174a25a3f6789
abekah.ekow <abekah.ekow@gmail.com>
2023-06-30 16:56:14 +0000

    update3 horizontal scroll button to awards component

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -50,12 +50,12 @@ const data =[{
 export default function Awards() {
     const leftScroll = () => {
         var slider = document.getElementById('slider');
-        slider.scrollLeft= slider.scrollLeft - 500;
+        slider.scrollLeft= slider.scrollLeft - 200;
       
     };
      const rightScroll = () => {
         var slider = document.getElementById('slider');
-        slider.scrollLeft= slider.scrollLeft + 500;
+        slider.scrollLeft= slider.scrollLeft + 200;
   
     };
     const eachaward = data.map(item => {
@@ -63,21 +63,21 @@ export default function Awards() {
         return (
 
             <div key={item.id}
-                className="snap-center hover:scroll-auto flex-shrink-0 w-[220px] h-[80%] mt-20 mb-10 drop-shadow-md rounded-lg transition ease-in-out delay-150 
+                className="snap-center hover:scroll-auto flex-shrink-0 w-[200px] md:w-[220px] md:h-[80%] mt-20 mb-10 drop-shadow-md rounded-lg transition ease-in-out delay-150 
                  bg-stone-200 hover:-translate-y-1 hover:scale-105 duration-150 relative  border-1 
                     border-gray-300 shadow-md hover:bg-gray-200 hover:shadow-md hover:shadow-cyan-600"
             >
                 <div
-                    className="rounded-full border-gray-400 border shadow-md  text-sm text-center absolute top-[-2px] left-[-1px] 
-                w-20 h-8 font-semibold bg-slate-400 m-3 p-1 text-gray-800"
+                    className="rounded-full border-gray-400 border shadow-sm shadow-cyan-600 text-sm text-center absolute top-[-2px] left-[-1px] 
+                w-18 h-7 md:w-20 md:h-8 font-semibold bg-slate-500 m-3 p-1 text-gray-200"
                 >
                   {item.year}
                 </div>
 
 
-                <div className="w-full h-[280px]">
+                <div className="w-full h-[250px]">
                     <Image
-                        className="rounded-md object-cover w-full h-full shadow-lg border border-slate-400"
+                        className="rounded-md object-cover w-full h-full shadow shadow-cyan-600 border border-slate-400"
                         src={item.img}
                         alt={item.img}
                         width={300}
@@ -92,15 +92,15 @@ export default function Awards() {
                     
                     <div className="space-y-2">
                         
-                        <p className=" overflow-x-visible text-gray-700  font-semibold text-md w-full h-5">
+                        <p className=" overflow-x-visible text-gray-700  font-semibold text-sm md:text-md w-full h-5">
                             {item.description}
                         </p>
                     </div>
-                    <div className="flex space-x-4">
+                    {/* <div className="flex space-x-4">
                         <div className="animate-pulse w-1/3 h-3 bg-sky-300"></div>
                         <div className="animate-pulse w-1/3 h-3 bg-sky-300"></div>
                         <div className="animate-pulse w-1/3 h-3 bg-sky-300"></div>
-                    </div>
+                    </div> */}
                 </div>
 
 
@@ -114,14 +114,14 @@ export default function Awards() {
 
 
     return (
-       <div  className=" relative flex w-[95%] md:w-[70%] items-center  justify-center">
+       <div  className=" relative flex w-[100%] md:w-[70%] items-center  justify-center">
           <ChevronLeftIcon onClick={leftScroll} className="  text-cyan-600 hover:text-cyan-500 hover:scale-125 transition duration-300 w-20 cursor-pointer"/> 
         
         <section id='slider' className="w-[100%] h-[100%] scroll-smooth overflow-x-scroll snap-x
                              snap-mandatory  
                             whitespace-nowrap text-gray-800 mt-4 ">
                                
-            <div className="flex ml-8 w-full h-full space-x-6 flex-row">
+            <div className="flex ml-8 w-full h-full space-x-8 flex-row">
                 {eachaward}
             </div>
            
